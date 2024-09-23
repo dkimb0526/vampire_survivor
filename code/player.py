0625709fd40c1f0be923b59567812abce75e0bd0
david <xingkimb@gmail.com>
2024-09-23 10:27:18 -0400

    organized player class with  input and move method

--- code/player.py
+++ code/player.py
@@ -11,10 +11,18 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.Vector2()
         self.speed = 300
 
-    def update(self, dt):
+    def input(self):
         #return a list of boolean that returns true of the key is pressed at the index of list
         keys = pygame.key.get_pressed()
         #if returns true for Right then 1-0, else 0-1 for left.
         self.direction.x = int(keys[pygame.K_d] - int(keys[pygame.K_a]))
         self.direction.y = int(keys[pygame.K_s] - int(keys[pygame.K_w]))
+        self.direction = self.direction.normalize()
+
+    def move(self, dt):
         self.rect.center += self.direction * self.speed * dt
+
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
