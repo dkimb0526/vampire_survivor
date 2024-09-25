dcbefdad489a300de358be4f76867781dab92782
david <xingkimb@gmail.com>
2024-09-25 11:15:53 -0400

    updated collision sprites and imported objects

--- code/main.py
+++ code/main.py
@@ -20,22 +20,21 @@ class Game:
         #groups
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
+
+        self.setup()
+
         #sprites
         self.player = Player((400,300), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60,100), randint(50,100)
-            CollisionSprite((x,y), (w,h), (self.all_sprites, self.collision_sprites))
-        self.setup()
-    def setup(self):
-        map = load_pygame(join("..","data","maps","world.tmx"))
 
-        
+    def setup(self):
+        #import map objects
+        map = load_pygame(join("..","data","maps","world.tmx"))
         
         for obj in map.get_layer_by_name("Objects"):
-            print(obj.x)
-            print(obj.y)
-            print(obj.image)
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+            #print(obj.x)
+            #print(obj.y)
+            #print(obj.image)
 
 
 
